5d4e1ff56407a632099cf5712584ab1e21e1191c
jvcm <joaozinhocavalcante.jovc@gmail.com>
2026-05-05 13:32:54 -0300

    quickstart - vite ?

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const bodyElement = document.querySelector("body");
+bodyElement.textContent = "Hello World!";
+/*function App() {
+     Não é react...
+    return(
+      <>
+        <h1>
+          hello ts
+        </h1>
+      </>
+    );
+    
+}*/
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+const bodyElement = document.querySelector("body") as HTMLElement;
+
+bodyElement.textContent= "Hello World!";
+
+
+/*function App() {
+     Não é react...
+    return(
+      <>
+        <h1>
+          hello ts
+        </h1>
+      </>
+    );
+    
+}*/
