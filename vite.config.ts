a492107cde9cd92ee734c84ef420a805d2b58b93
jvcm <joaozinhocavalcante.jovc@gmail.com>
2026-05-12 21:30:20 -0300

    basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
 
 export default defineConfig({
+  base: "/blog/",
   plugins: [react(), mdPlugin({ mode: [Mode.FRONTMATTER, Mode.HTML] })],
 });
